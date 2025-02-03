97a1cf68f1e8edbb5b3d52c633cfce1a36d22bbe
proMahadi <professionalmahadi@gmail.com>
2025-02-03 19:27:58 +0600

    minor bug fixed

--- src/components/shared/Navbar/index.tsx
+++ src/components/shared/Navbar/index.tsx
@@ -6,8 +6,8 @@ import logo  from "@/assets/logo.webp"
 const Navbar:React.FC = () => {
   return (
     <section className='pt-8 xl:pt-[62px] absolute top-0 left-0 z-10 w-full'>
-        <Image src={logo} alt="logo" className='mx-auto xl:block hidden'/>
-        <Image src={logo} alt="logo" className='mx-auto w-[163px] xl:hidden'/>
+        <Image src={logo} alt="logo" className='mx-auto lg:block hidden'/>
+        <Image src={logo} alt="logo" className='mx-auto w-[163px] lg:hidden'/>
     </section>
   )
 }
