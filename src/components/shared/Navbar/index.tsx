b2de0ac57992a0c262201987e072de71b4d90a84
proMahadi <professionalmahadi@gmail.com>
2025-02-09 21:26:11 +0600

    navbar logo updated

--- src/components/shared/Navbar/index.tsx
+++ src/components/shared/Navbar/index.tsx
@@ -1,12 +1,12 @@
 import Image from 'next/image'
 import React from 'react'
-import logo  from "@/assets/logo.webp"
+import logo  from "@/assets/logo.svg"
 
 
 const Navbar:React.FC = () => {
   return (
     <section className='pt-8 xl:pt-[62px] absolute top-0 left-0 z-10 w-full'>
-        <Image src={logo} alt="logo" className='mx-auto lg:block hidden'/>
+        <Image src={logo} alt="logo" className='mx-auto lg:block hidden w-[233px]'/>
         <Image src={logo} alt="logo" className='mx-auto w-[163px] lg:hidden'/>
     </section>
   )
