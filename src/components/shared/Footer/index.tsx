9d69dd405f99dcb77be0833c70d0b9a079bfdf92
proMahadi <professionalmahadi@gmail.com>
2025-02-12 12:42:52 +0600

    footer logo replaced

--- src/components/shared/Footer/index.tsx
+++ src/components/shared/Footer/index.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import React from "react";
-import footerLogo2 from "@/assets/footerLogo.svg";
+import footerLogo from "@/assets/footerLogo.png";
 import { FaFacebook } from "react-icons/fa";
 import { RiInstagramFill } from "react-icons/ri";
 import { FaLinkedin } from "react-icons/fa";
@@ -9,7 +9,7 @@ const Footer: React.FC = () => {
   return (
     <section className="bg-black pt-12 pb-[59px] md:pt-[67px] md:pb-8">
       <div>
-        <Image src={footerLogo2} alt="footerLogo" className="mx-auto w-auto h-auto sm:h-[84px] sm:w-[84px] object-cover" />
+        <Image src={footerLogo} alt="footerLogo" className="mx-auto h-[84px] w-[84px] object-cover" />
         <p className=" font-figTree font-normal text-xl text-white mt-2.5 text-center">
           Montreal, QC
         </p>
